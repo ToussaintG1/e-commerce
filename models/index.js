f84a52fab62e90a2e388c18e2fe1f37763fd68ae
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-09-01 17:38:17 -0500

    added Product.js and category models, added Index.js api

--- models/index.js
+++ models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Book.belongsTo(Reader, {
+  foreignKey: 'reader_id',
+});
 // Categories have many Products
-
+Reader.hasMany(Book, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Book.belongsTo(Reader, {
+  through: {
+    model: Trip,
+    unique: false
+  },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Book.belongsTo(Reader, {
+  through: {
+    model: Trip,
+    unique: false
+  },
+});
 module.exports = {
   Product,
   Category,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,13 +3,26 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoriesData = await Category.findAll({
+    include: [{ model: Product }],
+  });
+  res.status(200).json(categoriesData);
+} catch (err) {
+  res.status(500).json(err);
+}
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoriesData = await categoriesData.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+  }
   // be sure to include its associated Products
 });
 
