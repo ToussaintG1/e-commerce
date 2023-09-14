435e235f357f8ec18f0dca3687ec987792152cd7
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-09-14 09:17:00 -0500

    finished product-routes.js api file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    },
+  ],
+  })
+  .then ((products) => res.json(products))
+  .catch ((error) => res.status(400).json(error))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    indclude: [Category, {
+      model: Tag, 
+      through: ProductTag
+    },
+  ],
+    })
+    .then ((products) => res.json(products))
+    .catch ((error) => res.status(400).json(error))
+  });
 
 // create new product
 router.post('/', (req, res) => {
@@ -94,6 +115,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-});
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => res.json(product))
+    .catch ((error) => res.status(500).json(error))
+  });
 
 module.exports = router;
